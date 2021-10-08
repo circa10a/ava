b3587f9d9e1c8fb2768ee9686d88d4a01ea8aa7a
Caleb Lemoine <21261388+circa10a@users.noreply.github.com>
2021-10-08 10:12:59 -0500

    add video command (#16)

--- README.md
+++ README.md
@@ -56,6 +56,7 @@ npm start
 - `mock` - `ava mock <text>` reports back spongebob "mock" formatted text.
 - `sandwich` - `ava sandwich` will make you a sandwich of random food items.
 - `takemehome` - `ava takemehome` sings the John Denver classic.
+- `video` - `ava video something something` returns a youtube video based on the search terms provided.
 - `whiteclaw` - `ava whiteclaw` does the needful.
 - `yeet` - `ava yeet` sends an awesome yeet pic.
 
---
+++ commands/video.js
@@ -0,0 +1,34 @@
+const usetube = require('usetube');
+const { avaPrefix } = require('../config/config');
+
+const path = require('path');
+const fileName = path.basename(__filename);
+const command = fileName.replace('.js', '');
+
+module.exports = {
+  commandName: command,
+  name: 'messageCreate',
+  once: false,
+  execute: async(message) => {
+    // Ensure message is intended for ava
+    if (!message.content.toLowerCase().startsWith(avaPrefix)) {
+      return;
+    }
+    const args = message.content.trim().split(/ +/g);
+    const userCmd = args[1];
+    const query = args.slice(2, args.length).join(' ');
+
+    if (userCmd === command) {
+      if (!query) {
+        message.reply('No search terms provided');
+        return;
+      }
+      try {
+        results = await usetube.searchVideo(query);
+        message.reply(`https://www.youtube.com/watch?v=${results.videos[0].id}`);
+      } catch(e) {
+        message.channel.send(`\`\`\`log\n${e.toString()}\`\`\``);
+      }
+    };
+  }
+};
