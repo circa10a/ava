5d8aca2486834fe4343e33457ab419a2b962cfad
Traci Kamp <traci.kamp@gmail.com>
2021-10-13 20:07:17 -0700

    refactor(fuck): use randomItemFromArray utility

--- commands/foaas.js
+++ commands/foaas.js
@@ -1,5 +1,6 @@
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 const { avaPrefix } = require('../config/config');
+const { randomItemFromArray } = require('../lib/utils/utils');
 const foaasRootEndpoint = 'https://foaas.com';
 const fuck = 'fuck';
 
@@ -26,10 +27,10 @@ module.exports = {
         // Get the list of operations (filtered)
         const ops = await fetch('https://foaas.com/operations').
           then(response => response.json()).
-          then(data => data.filter(item => item.fields.length === 2 && item.url.includes(':name') && item.name!='Awesome' && item.name!='Life' &&item.name!='Yeah'));
+          then(data => data.filter(item => item.fields.length === 2 && item.url.includes(':name') && item.name!='Awesome' && item.name!='Life' && item.name!='Yeah'));
 
         // Pick a random one
-        const selection = ops[Math.floor(Math.random() * ops.length)];
+        const selection = randomItemFromArray(ops)
 
         // Do the needful
         const selectedUrl = selection.url.replace(':name', args[2]).replace(':from', message.author.name);
